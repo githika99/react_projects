f938046bd414368e12bf9e57a2b228b88e3e4636
Githika <Githika.an@gmail.com>
2023-12-21 17:52:17 -0800

    js as pure language  init

---
+++ javascript-as-purelanguage/hw.js
@@ -0,0 +1,20 @@
+// content https://javascript.info/structure
+
+console.log('Hi there ...');
+
+let user = 'John';
+let age = 25;
+let message = 'Hello';
+
+console.log(user, age,message);
+
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
