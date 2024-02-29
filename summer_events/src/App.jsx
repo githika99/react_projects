23bf976fd160a5594503fe7b6fd22035abf115f9
Githika <Githika.an@gmail.com>
2024-02-28 21:02:42 -0800

    final push, all functionalities implemented

--- summer_events/src/App.jsx
+++ summer_events/src/App.jsx
@@ -4,8 +4,10 @@ import Grid from './components/Grid';
 function App() {
   return (
     <div className = "App">
+      <img class = 'beach_ball' src = 'src/assets/beach_ball.jpeg'></img>
+      <img class = 'palm_tree' src = 'src/assets/palmtree.jpeg'></img>
       <h1>Summer Events in the Bay Area</h1>
-      <h3>A list of free, outside events happening in the San Francisco Bay Area in Summer 2024!</h3>
+      <h3>A list of free, outdoor events happening in the San Francisco Bay Area in Summer 2024!</h3>
       <Grid> </Grid>
     </div>
   )
--- summer_events/src/components/Event.jsx
+++ summer_events/src/components/Event.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 
 const Event = (props) => {
+    const handleClick = () => {
+        window.location.href = props.link;
+    };
+
+      
     return (
         <td className = {'Event ' + props.color}>
             <h3>{props.event}</h3>
             <h6>{props.location} </h6>
             {/* <h10> <br /> </h10> */}
-            <h5>{props.date_time}</h5>
+            <h5>{props.date}</h5>
+            <h5>{props.time}</h5>
             <img src= {props.image} ></img>
-            {/* <Link to={props.link}>
-                <button className="my-button">Visit Website</button>
-            </Link> */}
+            <button className="my-button" onClick={handleClick}>Visit Website</button>
+         
+            
         </td>
 
     )
