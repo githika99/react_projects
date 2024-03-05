564b664d25d8a83330e24ecf056289c1816a6815
subba reddy <asubbareddy@gmail.com>
2024-03-05 00:05:25 -0800

    "react router" concepts are  taken from this React Router tutorial

--- using_router/src/main.jsx
+++ using_router/src/main.jsx
@@ -1,25 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { BrowserRouter } from "react-router-dom";
+
+import { Link, Routes, Route } from "react-router-dom";
 
 import SamosaKitchen from "./pages/SamosaKitchen.jsx";
 import TestPage from "./pages/TestPage.jsx";
 
 import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <SamosaKitchen />,
-  },
-  {
-    path: "/test",
-    element: <TestPage />,
-  },
-]);
+// concepts are  taken from this React Router tutorial
+// https://github.com/remix-run/react-router/blob/main/examples/basic/src/App.tsx
 
+// ------------------
+function App() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/kitchen">Kitchen...</Link>
+          </li>
+          <li>
+            <Link to="/testroute">Test Page</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/kitchen" element={<SamosaKitchen />} />
+        <Route path="/testroute" element={<TestPage />} />
+      </Routes>
+    </>
+  );
+}
+
+// --------------------
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
