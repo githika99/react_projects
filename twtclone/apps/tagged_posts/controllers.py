95a3c7990806ce3211d9d264f203c2118141fd80
gita-admin <asubbareddy@gmail.com>
2024-05-17 00:25:00 -0700

    added  routes api/tags  api/posts

--- twtclone/apps/tagged_posts/controllers.py
+++ twtclone/apps/tagged_posts/controllers.py
@@ -40,3 +40,52 @@ def index():
     user = auth.get_user()
     message = T("Hello {first_name}").format(**user) if user else T("Hello")
     return dict(message=message)
+
+
+# ----------------------- main project code ----------------------------------                                                                    
+#-------------------------                  ----------------------------------
+@action("api/tags", method="GET")
+def tags():
+    return {"tags": ["news", "technology", "sports"] }
+
+
+@action("api/posts", method="GET")
+def posts():
+
+    return {"posts": [
+        {"content": "Sky is going to drop to night..... :-).", "created_on": "05/01/2024", "created_by": "Easy Peasy Gir"},
+        {"content": "Sky is going to drop to night..... :-).", "created_on": "05/01/2024", "created_by": "Easy Peasy Gir"}
+    ]
+    }
+
+
+@action("api/posts", method="POST")
+def posts():
+    tweet_text = request.forms.get('tweet_text')
+
+    return {"posts": "----  22we need to insert this NEW POST into database -----------", "tweet": {"content": tweet_text, "created_on": "05/01/2024", "created_by": "Easy Peasy Gir" }}
+
+# ---------- basic test funcitons to test different ROUTES ------------------------------------------------------
+import datetime
+from py4web import action , response, request
+
+
+@action('index2')
+def page():
+    return "Welcome to my Twitter clone web site .. TIME now is %s" % datetime.datetime.now()
+
+@action('timenow')
+def page():
+    return "Time now is  %s" % datetime.datetime.now()
+
+@action("testpage")
+@action.uses("page1.html")
+def page1():
+    return {"name": "Gitu ", "age": 16}
+
+
+@action('color/<name>')
+def color(name):
+    if name in ['red', 'blue', 'green']:
+        return 'You picked color %s' % name
+    return 'Unknown color %s' % name
